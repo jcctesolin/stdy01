cc740583ce59cefc6219b808f5521ba27375c12a
Julio Tesolin <jcctesolin@gmail.com>
2024-03-07 19:44:48 -0300

    modfied gufo import in hint-o

--- ontoimport/importTest.py
+++ ontoimport/importTest.py
@@ -2,11 +2,11 @@ import owlready2 as owlr
 
 #onto = owlr.get_ontology("http://test.org/onto.owl")
 #o_hint = owlr.get_ontology("file:///home/dev/workdir/stdy01/ontoimport/onto/hint-r.rdf").load()
-o_hint = owlr.get_ontology("https://raw.githubusercontent.com/jcctesolin/stdy01/dev/ontoimport/onto/hint-r.rdf").load()
+o_hint = owlr.get_ontology("https://raw.githubusercontent.com/jcctesolin/stdy01/dev/ontoimport/onto/hint-o.owl").load()
 #o_qvas = owlr.get_ontology("file:///home/dev/workdir/stdy01/ontoimport/onto/qvas-r.rdf").load()
 #o_miscon = owlr.get_ontology("file:///home/dev/workdir/stdy01/ontoimport/onto/owlBase03112023.owl").load()
 #o_gufo = owlr.get_ontology("file:///home/dev/workdir/stdy01/ontoimport/onto/gufo-r.rdf").load()
-o_gufo = owlr.get_ontology("https://raw.githubusercontent.com/jcctesolin/stdy01/dev/ontoimport/onto/gufo-r.rdf").load()
+o_gufo = owlr.get_ontology("https://raw.githubusercontent.com/jcctesolin/stdy01/dev/ontoimport/onto/gufo-o.owl").load()
 
 o_hint.name = "hint"
 #o_qvas.name="qvas"
